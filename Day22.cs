e1c252f79732a08b4fd1fc84c7f4bd6bbe119abe
irenereisner <irene@miratrix.at>
2022-12-28 21:00:11 +0100

    add empty remaining days

---
+++ Day22.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    [Day(22)]
+    public class Day22 : Day
+    {
+        public override string RunPart1()
+        {
+            return "";
+        }
+        public override string RunPart2()
+        {
+            return "";
+        }
+
+    }
+}
---
+++ Day25.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    [Day(25)]
+    public class Day25 : Day
+    {
+        public override string RunPart1()
+        {
+            return "";
+        }
+        public override string RunPart2()
+        {
+            return "";
+        }
+
+    }
+}
